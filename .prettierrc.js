0666775ed4b74fbbe63959a25d1dc41f694f1194
Alexander Sosov <a.sosov@vlig44.ru>
2024-04-23 12:31:06 +0300

    chore

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,11 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
-};
+  tabWidth: 2,
+  semi: false,
+  indent_style: "space",
+  indent_size: 2,
+  max_line_length: 80,
+  jsxSingleQuote: false,
+  endOfLine: "auto"
+}
