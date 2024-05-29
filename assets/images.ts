acf14691454eaeb552f1a9ac504b17fdf0cda792
Alexander Sosov <a.sosov@vlig44.ru>
2024-05-29 17:09:28 +0300

    feat: добавить информацию об авторизованном пользователе на главном экране

--- assets/images.ts
+++ assets/images.ts
@@ -1,5 +1,9 @@
+//@ts-ignore
 import personBg from './images/man.png'
+//@ts-ignore
+import profileBg from './images/profile-bg.png'
 
 export const images = {
   personBg,
+  profileBg,
 }
--- screens/HomeTabScreen.tsx
+++ screens/HomeTabScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import {View, Text, ScrollView, Button} from 'react-native'
+import {View, Text, ScrollView, Button, ImageBackground} from 'react-native'
 import auth from '@react-native-firebase/auth'
 
 import {useUser} from '../hooks/useUser'
 
 import type {TabStack} from '../types'
+
+import {images} from '../assets/images'
 
 const HomeTabScreen = ({}: TabStack.HomeScreenProps) => {
   const [user] = useUser()
@@ -17,9 +19,21 @@ const HomeTabScreen = ({}: TabStack.HomeScreenProps) => {
         padding: 26,
       }}>
       <View className="flex flex-1">
-        <Text className="text-white font-inter">
-          Добро пожаловать {user?.email}
-        </Text>
+        <View className="flex flex-row justify-between">
+          <View>
+            <Text className="text-accent font-inter">Добро пожаловать!</Text>
+            <Text className="text-xl font-psemi text-white -mt-1">
+              Александр
+            </Text>
+          </View>
+          <View>
+            <ImageBackground
+              source={images.profileBg}
+              resizeMode="contain"
+              className="w-[48px] h-[48px] border-r-[50%]"
+            />
+          </View>
+        </View>
         <Button
           title="Выйти"
           onPress={async () => {
